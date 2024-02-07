0e7b4a6b5c6455ccb9339d13ea1f8385a4d02b10
danunnaise <danunai1993@gmail.com>
2024-02-07 20:27:21 +0700

    Added instructions at README.md

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # ccminer verus autostart termux linux(ubuntu)
+
+```
+termux-setup-storage
+```
+```
+pkg update -y
+```
+ต่อจากนี้ให้ตอบ N ทั้งหมด
+
+```
+pkg install proot-distro -y && proot-distro install ubuntu && pkg install nano && pkg install wget -y && pkg install python3 && cd /data/data/com.termux/files/usr/etc && nano profile
+```
+
+เลื่อนลงไปล่างสุด ใส่คำสั่ง
+```
+proot-distro login ubuntu
+```
+
+```
+apt-get update -y && apt-get install git -y && git clone https://github.com/danunaise/ccminer-termux.git && cd AUTORUN-CCMINER-X1.2 && chmod +x setup.sh && sh setup.sh
+```
+
+หลังเปิด bash.bashrc
+เพิ่มคำสั่ง startminer
+ctrl+x ตอบ yes เพื่อ save และ กด enter เพื่อออก
+
+หลังทำเสร็จให้ กดค้างที่หน้าจอ เลือกที่ more... และ kill process
+ที่สำคัญอย่าลืม กดเปิด Keep screen on ไว้เพื่อให้โปรแกรมขุดทำงานตลอดโดยที่ไม่พักหน้าจอ
--- mining/config.py
+++ mining/config.py
@@ -1,17 +1,16 @@
 import os
 import json
-from colored import Fore, Back, Style
 #config worker
 
 def main():
     while True:
-        print(f"{Fore.blue}*******************************************{Style.reset}")
+        print("\033[34m*******************************************\033[0m")
         print("กรุณาเลือกเมนูที่ต้องการ")
         print("1. ตั้งค่า API")
         print("2. ตั้งค่า Worker")
         print("3. ออกและเริ่มขุด")
         print("4. ออกจากการตั้งค่า")
-        print(f"{Fore.blue}*******************************************{Style.reset}")
+        print("\033[34m*******************************************\033[0m")
         
         try:
             menu = int(input("เลือกเมนู: "))
@@ -22,26 +21,26 @@ def main():
                 editWorker()
             elif menu == 3:
                 os.system('cls || clear')
-                print(f"{Fore.light_green}กำลังเริ่มขุด...{Style.reset}")
-                os.system('python3 main.py')
+                print("\033[92mกำลังเริ่มขุด...\033[0m")
+                os.system('startminer')
                 break
             elif menu == 4:
                 os.system('cls || clear')
-                print(f"{Fore.red}ออกจากโปรแกรม{Style.reset}")
+                print("\033[31mออกจากโปรแกรม\033[0m")
                 break
             else:
                 os.system('cls || clear')
-                print(f"{Fore.white}{Back.red} ERROR {Style.reset} {Fore.red}กรุณาเลือกเมนูใหม่{Style.reset}")
+                print("\033[1;37;41m ERROR \033[0m \033[31mกรุณาเลือกเมนูใหม่\033[0m")
         except ValueError:
             os.system('cls || clear')
-            print(f"{Fore.white}{Back.red} ERROR {Style.reset} {Fore.red}กรุณาป้อนตัวเลขเท่านั้น{Style.reset}")
+            print("\033[1;37;41m ERROR \033[0m \033[31mกรุณาป้อนตัวเลขเท่านั้น\033[0m")
             
 def setupApi():
     os.system('cls || clear')
     with open('./data/api.json', 'r') as f:
         url_data = json.load(f)
         url = url_data.get('url')
-    print(f"{Fore.blue}*******************************************{Style.reset}")
+    print("\033[34m*******************************************\033[0m")
 
     if url is None:
         print("API ของคุณตอนนี้คือ: ยังไม่มี API กรุณาตั้งค่า API ก่อน")
@@ -50,7 +49,7 @@ def setupApi():
     print("กรุณากรอก API ที่มีข้อมูล pool, wallet, password ")
     print("เช่น https://raw.githubusercontent.com/danunaise/ccminer-verus-autostart/main/data/data.json")
     
-    print(f"{Fore.blue}*******************************************{Style.reset}")
+    print(f"\033[34m*******************************************\033[0m")
     url = input("กรุณากรอก API ของคุณ: ")
     data = {
         "url": url
@@ -58,23 +57,23 @@ def setupApi():
     with open('./data/api.json', 'w') as f:
         json.dump(data, f, indent=1)
         os.system('cls || clear')
-        print(f"{Fore.white}{Back.green} SUCCESS {Style.reset}{Fore.green} บันทึกข้อมูล API เรียบร้อยแล้ว{Style.reset}")
+        print(f"\033[1;37;42m SUCCESS \033[0m\033[32m บันทึกข้อมูล API เรียบร้อยแล้ว\033[0m")
 
 def editWorker():
     os.system('cls || clear')
     with open('./data/worker.json', 'r') as f:
         worker_data = json.load(f)
         worker = worker_data.get('worker')
-    print(f"{Fore.blue}*******************************************{Style.reset}")
+    print(f"\033[34m*******************************************\033[0m")
     print("worker ของคุณตอนนี้คือ " + worker)
     print("กรุณากรอกข้อมูล worker ของคุณ เช่น miner1")
-    print(f"{Fore.blue}*******************************************{Style.reset}")
+    print(f"\033[34m*******************************************\033[0m")
     worker = input("ชื่อ worker ของคุณ: ")
 
     #config cpu_threads
-    print(f"{Fore.blue}*******************************************{Style.reset}")
+    print(f"\033[34m*******************************************\033[0m")
     print("จำนวน cpu_threads ที่ต้องการใช้ขุด ใส่ค่า 0 - 8")
-    print(f"{Fore.blue}*******************************************{Style.reset}")
+    print(f"\033[34m*******************************************\033[0m")
     cpu_threads = input("cpu_threads ของคุณ: ")
 
     #save data
@@ -86,7 +85,7 @@ def editWorker():
     with open('./data/worker.json', 'w') as f:
         json.dump(data, f, indent=2)
         os.system('cls || clear')
-        print(f"{Fore.white}{Back.green} SUCCESS {Style.reset}{Fore.green} บันทึกข้อมูล Worker เรียบร้อยแล้ว{Style.reset}")
+        print(f"\033[1;37;42m SUCCESS \033[0m\033[32m บันทึกข้อมูล Worker เรียบร้อยแล้ว\033[0m")
 
 if __name__ == "__main__":
     main()
